bcd90a1a5b81e3cb2715a0ddd2579c334bc42715
Mark Thomas <markt@apache.org>
2023-11-08 16:36:41 +0000

    Use sendError() rather than working directly with Coyote response
    The primary benefit is that the standard error page mechanism is
    invoked.

--- java/org/apache/catalina/connector/InputBuffer.java
+++ java/org/apache/catalina/connector/InputBuffer.java
@@ -300,7 +300,6 @@ public class InputBuffer extends Reader
      *
      * @throws IOException An underlying IOException occurred
      */
-    @SuppressWarnings("deprecation")
     @Override
     public int realReadBytes() throws IOException {
         if (closed) {
@@ -317,25 +316,26 @@ public class InputBuffer extends Reader
         try {
             return coyoteRequest.doRead(this);
         } catch (BadRequestException bre) {
-            // Set flag used by asynchronous processing to detect errors on non-container threads
-            coyoteRequest.setErrorException(bre);
-            // In synchronous processing, this exception may be swallowed by the application so set error flags here.
-            coyoteRequest.setAttribute(RequestDispatcher.ERROR_EXCEPTION, bre);
-            coyoteRequest.getResponse().setStatus(400);
-            coyoteRequest.getResponse().setError();
             // Make the exception visible to the application
+            handleReadException(bre);
             throw bre;
         } catch (IOException ioe) {
-            // Set flag used by asynchronous processing to detect errors on non-container threads
-            coyoteRequest.setErrorException(ioe);
-            // In synchronous processing, this exception may be swallowed by the application so set error flags here.
-            coyoteRequest.setAttribute(RequestDispatcher.ERROR_EXCEPTION, ioe);
-            coyoteRequest.getResponse().setStatus(400);
-            coyoteRequest.getResponse().setError();
+            handleReadException(ioe);
             // Any other IOException on a read is almost always due to the remote client aborting the request.
             // Make the exception visible to the application
             throw new ClientAbortException(ioe);
         }
+    }
+
+
+    private void handleReadException(Exception e) throws IOException {
+        // Set flag used by asynchronous processing to detect errors on non-container threads
+        coyoteRequest.setErrorException(e);
+        // In synchronous processing, this exception may be swallowed by the application so set error flags here.
+        Request request = (Request) coyoteRequest.getNote(CoyoteAdapter.ADAPTER_NOTES);
+        Response response = request.getResponse();
+        request.setAttribute(RequestDispatcher.ERROR_EXCEPTION, e);
+        response.sendError(400);
     }
 
 
