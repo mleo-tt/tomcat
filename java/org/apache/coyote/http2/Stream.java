a312e41705d01a3319995f951931d0ce66466869
Mark Thomas <markt@apache.org>
2024-01-25 06:20:14 -0600

    Make recycled streams eligible for GC immediately. Improves scalability.

--- java/org/apache/coyote/http2/Stream.java
+++ java/org/apache/coyote/http2/Stream.java
@@ -492,7 +492,7 @@ class Stream extends AbstractNonZeroStream implements HeaderEmitter {
         if (headerException == null) {
             return;
         }
-
+        handler.getHpackDecoder().setHeaderEmitter(Http2UpgradeHandler.HEADER_SINK);
         throw headerException;
     }
 
